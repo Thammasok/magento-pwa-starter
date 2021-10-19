5791f948768a0fda7484fb520b71a6b4f9a2c5ba
Jaranchai.to <jaranchai.nt@hotmail.com>
2021-10-20 06:58:57 +0700

    change file

--- src/@Scg/src/routers/index.js
+++ src/@Scg/src/routers/index.js
@@ -2,11 +2,11 @@ module.exports = () => [
   {
     name: 'MyGreetingRoute',
     pattern: '/greeting/:who?',
-    path: require.resolve('../views/Greeting/index.jsx')
+    path: require.resolve('../views/Greeting/index.js')
   },
   {
     name: 'TodoRoute',
     pattern: '/todo',
-    path: require.resolve('../views/Todo/index.jsx')
+    path: require.resolve('../views/Todo/index.js')
   }
 ]
